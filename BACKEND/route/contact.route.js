28b1c1c3e5b9daff005e4110878351eebfebae8f
Narendra Prasad <163150852+Narendraprasad2811@users.noreply.github.com>
2025-04-17 17:18:30 +0530

    Delete BACKEND directory

--- BACKEND/route/contact.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { submit } from "../Controller/contact.controller.js";
-
-const router = express.Router();
-
-router.post('/contact', submit);
-
-export default router;
--- BACKEND/route/user.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { signup, login } from "../Controller/user.controller.js";
-const router = express.Router();
-
-router.post("/signup", signup);
-router.post("/login", login);
-
-export default router;
